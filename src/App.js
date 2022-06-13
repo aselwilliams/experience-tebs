1bfd37e03fde7f5bd7ca089c6d0aa8adaf9d6d25
aselwilliams <aselwilliams22@gmail.com>
2022-06-13 19:47:20 -0400

    styles added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react';
-import {FaSpinner} from 'react-icons/fa'
+import {FaSpinner,FaAngleDoubleRight} from 'react-icons/fa'
 import './App.css';
 const url='https://course-api.com/react-tabs-project'
 
@@ -17,13 +17,40 @@ function App() {
 
   useEffect(() => {
     getData()
-  })
-const {company, dates, title, duties} =jobs[index]
+  },[])
+  if(loading) {
+    return (
+<section className='section loading'><FaSpinner /></section>
+    )
+  }
+   const {company, duties, title, dates} =jobs[index]
   return (
-    <section className="App">
-      {loading && <section className='section loading'><FaSpinner /></section>}
-
-  
+    <section className="container">
+    <header>
+      <h1 className='title'>Experience</h1>
+     <div className='underline'></div>
+     </header>
+     <main className='main'>
+    
+      <div className='btn-container'>
+        {jobs.map((item, ind)=>(
+          <button key={ind} onClick={()=>setIndex(ind)} className='btn'>{item.company}</button>
+        ))}
+      </div>
+     <article className='job-info'>
+          <h3 className='position'>{title}</h3>
+          <h4 className='company'>{company}</h4>
+          <p className='dates'>{dates}</p>
+       {duties.map((duty,ind)=>{
+         return (
+           <div className='duty-row' key={ind}>
+             <FaAngleDoubleRight className='job-icon' />
+             <p>{duty}</p>
+           </div>
+         )
+       })}
+     </article>
+     </main>
     </section>
   );
 }
