6a76ed80cff1f5348d422484b9a5a7e2c9a6365e
aselwilliams <aselwilliams22@gmail.com>
2022-06-13 17:43:41 -0400

    fetched data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import {useState, useEffect} from 'react';
+import {FaSpinner} from 'react-icons/fa'
 import './App.css';
 const url='https://course-api.com/react-tabs-project'
-'
+
 function App() {
+  const [jobs, setJobs] = useState([]);
+  const [index, setIndex] =useState(0);
+  const [loading ,setLoading] = useState(true);
+
+  const getData=async ()=>{
+    const res= await fetch(url)
+    const data= await res.json()
+    setJobs(data)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    getData()
+  })
+const {company, dates, title, duties} =jobs[index]
   return (
-    <div className="App">
+    <section className="App">
+      {loading && <section className='section loading'><FaSpinner /></section>}
+
   
-    </div>
+    </section>
   );
 }
 
